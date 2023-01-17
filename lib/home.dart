a36bd43771a06a3867b3c0f6927be9f22a032c6e
Rusira Pathum <48381430+RusiraPathum@users.noreply.github.com>
2023-01-17 10:30:43 +0530

    add list view

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> data = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +22,7 @@ class _HomePageState extends State<HomePage> {
         title: Text('Home Page'),
       ),
       body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -29,6 +32,33 @@ class _HomePageState extends State<HomePage> {
                 child: Text(
                   'Welcome ${widget.userName}',
                   style: TextStyle(fontSize: 30),
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
+                  // scrollDirection: Axis.horizontal,
+                  itemCount: data.length,
+                  separatorBuilder: (context, index) {
+                    return Container(color: Colors.grey, height: 1);
+                  },
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue),
+                      child: Center(
+                        child: Text(
+                          data[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
               Container(
